c0a6d581e60cdac5d6246aed995130ab5bf13841
Nicolas Noriega <40644310+NickN18@users.noreply.github.com>
2019-11-04 10:15:11 -0700

    Add files via upload

---
+++ sprocket_v1.m
@@ -0,0 +1,72 @@
+%TODO:
+% MoveMotor() is utilized with a power set at 60% for a set distance that
+% we have yet to record. Only one wheel will be able to be put in reverse
+% while the other either stays in position or moves forward.
+clear all
+
+brick = ConnectBrick('EV33');
+
+while(1 == 1)
+    dir = input("Enter a direction (W: Go forward, A: Turn left, S: Go backwards, D: Turn right): ", 's');
+
+    %Port D is the clutch, Port B is the motor
+    
+    while(dir == 'A' || dir == 'a')
+         brick.MoveMotor('D', 30);
+         brick.MoveMotor('B', 100);
+    end
+    
+    while(dir == 'W' || dir == 'w')
+         brick.MoveMotor('D', -30);
+         brick.MoveMotor('B', -100);
+    end
+    
+    while(dir == 'S' || dir == 's')
+          brick.MoveMotor('D', -30);
+          brick.MoveMotor('B', 100);
+    end
+    
+    while(dir == 'D' || dir == 'd')
+          brick.MoveMotor('D', 30);
+          brick.MoveMotor('B', -100);     
+    end
+    
+    brick.StopMotor('D');
+    brick.StopMotor('B');
+
+end
+
+%As the robot moves, the drive motor pushes the clutch back and keeps it in
+%neutral
+
+%TODO: 
+%Gyro Control
+% The gyro sensor is controlled by the object gyroSensor. This angle should be
+% reset when there is a wall present. While the gyro should always stay at
+% zero (theoretically) it is be if we reset it to zero every time.
+
+%myGyro = gyroSensor(EV33, 'A');
+%angle = resetRotationAngle(myGyro);
+
+%TODO:
+%Maze Algorithm
+% The maze algorithm used will be the right wall follower. The ultrasonic
+% sensor will be positioned on the right side of the robot. 
+
+
+
+%TODO:
+% Clutch
+% The clutch is a bar with three gears at an angle where the last gear is
+% connecting to the forward gear. When you want to turn, the clutch is
+% flipped and the bar move at a different position where the reverse is
+% activated for only one wheel. The clutch doesn't restrict which wheel we
+% have to use, which mean it becomes a software issue, but something as
+% simple as determining which way to turn. Use MoveMotorAngleRel to
+% calculate the position of the lock-step straight motion and the reverse
+% pivoting motion so that I could just tell the program the move to the
+% exact position, or even set them to variables.
+
+%straightMotionAngle = MoveMotorAngleRel();
+
+
